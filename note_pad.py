8247ed652da12319a65dde083f6429fd614ae85d
David Omidoyin <davso4u@yahoo.com>
2025-09-18 18:58:50 +0100

    added gunicorn

--- note_pad.py
+++ note_pad.py
@@ -338,3 +338,6 @@ if __name__=="__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
